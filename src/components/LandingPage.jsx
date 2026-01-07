c2a09380aff605a94de142a34005eddbc9828342
Kazeem Oluwatimilehin <oluwatimilehinkazeem43@gmail.com>
2026-01-07 20:29:01 +0100

    centered a text

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,7 +1,7 @@
 
 const LandingPage = () => {
   return (
-    <div className='flex flex-col items-center justify-center h-screen text-3xl'>
+    <div className='flex flex-col items-center justify-center text-center h-screen text-3xl'>
         <h1 className='font-bold'>Welcome to Kazeem's Store</h1>
     </div>
   )
